3a5174b6c98d0d9bd1b2078a74fc90e4438b0fbf
KenStarry <starrycodes@gmail.com>
2025-01-19 12:46:14 +0300

    Minor Adjustments and updates

--- lib/flutter_extend.dart
+++ lib/flutter_extend.dart
@@ -5,6 +5,7 @@ import 'package:flutter/services.dart';
 
 /// DateTime Extensions
 part 'date_time/duration_extensions.dart';
+
 part 'date_time/date_time_extensions.dart';
 
 /// String Extensions
@@ -12,13 +13,12 @@ part 'string/global_string_extensions.dart';
 
 part 'string/auth_string_extensions.dart';
 
-part 'string/date_string_extensions.dart';
-
 /// Color Extensions
 part 'colors/global_color_extensions.dart';
 
 /// Widget Extensions
 part 'widgets/scaffold_extensions.dart';
+
 part 'widgets/text_extensions.dart';
 
 /// Context Extensions
