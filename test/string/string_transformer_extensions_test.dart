2a34e38975b355ebf22e8a39cc1d9cc01e675e64
KenStarry <starrycodes@gmail.com>
2025-03-09 12:40:15 +0300

    Added more String transformer extensions

--- test/string/string_transformer_extensions_test.dart
+++ test/string/string_transformer_extensions_test.dart
@@ -24,4 +24,42 @@ void main() {
       expect(text.withDefaultValue('no text'), 'no text');
     });
   });
+
+  group('Ordinals', (){
+    test("String with Ordinals eg. 1st, 2nd etc", () {
+      String text = '23';
+
+      expect(text.withOrdinal(), '23rd');
+    });
+
+    test("Non-Numeric string should not work but return the string", () {
+      String text = 'Ken 23';
+
+      expect(text.withOrdinal(), 'Ken 23');
+    });
+  });
+
+  test("Encode a String to URI format", () {
+    String text = 'hello world';
+
+    expect(text.withUrlEncoded(), 'hello%20world');
+  });
+
+  test("Add a Hashtag before a text", () {
+    String text = 'hello world';
+
+    expect(text.withHashtag(), '#helloworld');
+  });
+
+  test("Add brackets before a text", () {
+    String text = 'hello world';
+
+    expect(text.withBrackets('(', ')'), '(hello world)');
+  });
+
+  test("Add ellipsis if the text is too long", () {
+    String text = 'hello world';
+
+    expect(text.withEllipsis(8), 'hello wo...');
+  });
 }
