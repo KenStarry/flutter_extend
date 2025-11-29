c64041f37c68a4e8e93e8f2be1257e558bb8cd65
KenStarry <starrycodes@gmail.com>
2025-11-29 12:16:12 +0300

    feat: Add showSnackbar extension on BuildContext
    This commit introduces a `showSnackbar` extension method on `BuildContext`.
    
    The new extension simplifies showing a `SnackBar` by calling `ScaffoldMessenger.of(this).showSnackBar()`. It accepts a `Widget` for content and an optional `Duration`.

---
+++ lib/context/dialog_context_extensions.dart
@@ -0,0 +1,8 @@
+part of '../flutter_extend.dart';
+
+extension DialogContextExtensions on BuildContext {
+  void showSnackbar(Widget content,
+          {Duration duration = const Duration(seconds: 3)}) =>
+      ScaffoldMessenger.of(this)
+          .showSnackBar(SnackBar(content: content, duration: duration));
+}
