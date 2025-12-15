74d9da92efe7935e8c7bd2352cc2bb541660eef6
KenStarry <starrycodes@gmail.com>
2025-12-16 01:44:15 +0300

    Module files

---
+++ lib/files.dart
@@ -0,0 +1,7 @@
+library flutter_extend_files;
+
+/// File Extensions
+export 'src/extensions/files/file_extensions.dart';
+export 'src/extensions/files/file_type_extensions.dart';
+export 'src/extensions/files/file_size_check_extensions.dart';
+export 'src/extensions/files/file_operation_extensions.dart';
